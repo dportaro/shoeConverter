65c2792c8cda92ca99054a2df8ff51312b79bdab
David <davidportaro@gmail.com>
2014-09-28 22:23:20 +1000

    Shoe Converter App

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +27,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func covertButtonPressed(sender: UIButton)
+    {
+        let sizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        mensConvertedShoeSizeLabel.hidden = false
+        mensConvertedShoeSizeLabel.text = "\(sizeFromTextField + 30)" + " in European Shoe Size"
+        mensShoeSizeTextField.resignFirstResponder()
+   // This will run when convert button is pressed
+    }
+   
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton)
+    
+    {
+        let sizeFromTextField = Double ((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in Europeon Shoe Size"
+        womensShoeSizeTextField.resignFirstResponder()
+    // This will run once converted button is pressed
+    }
 }
 
